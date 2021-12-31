5c68674bb309569dedb610a44a037779cefb0062
Jose Castro <drakonc@gmail.com>
2021-12-31 11:51:00 -0500

    Inicio Segundo Video

--- app/Http/Livewire/Productos.php
+++ app/Http/Livewire/Productos.php
@@ -3,11 +3,16 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Producto;
 
 class Productos extends Component
 {
+    //TODO:Definimos variables para el componente
+    public $productos;
+
     public function render()
     {
+        $this->productos = Producto::all();
         return view('livewire.productos');
     }
 }
--- resources/views/livewire/productos.blade.php
+++ resources/views/livewire/productos.blade.php
@@ -1,3 +1,25 @@
-<div>
-    {{-- The whole world belongs to you. --}}
+<x-slot name="header">
+    <h1 class="text-gray-900">CRUD Con Laravel 8 y Liveeire </h1>
+</x-slot>
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-x1 sm:rounded-lg px-4 py-4">
+            <table class="table-fixed w-full">
+                <thead>
+                    <tr class="bg-red-600 text-white">
+                        <th class="px-4 py-2 w-2/12">ID</th>
+                        <th class="px-4 py-2">DESCRIPCION</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($productos as $producto)
+                    <tr>
+                        <td class="border px-4 py-2 text-center w-2/12">{{$producto->id}}</td>
+                        <td class="border px-4 py-2">{{$producto->descripcion}}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
