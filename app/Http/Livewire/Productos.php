fcb2d22501f6fa816348d18386567ba40b1f742a
Jose Castro <drakonc@gmail.com>
2022-01-28 15:25:27 -0500

    Add Function

--- app/Http/Livewire/Productos.php
+++ app/Http/Livewire/Productos.php
@@ -10,32 +10,44 @@ class Productos extends Component
     //TODO:Definimos variables para el componente
     public $productos, $descripcion, $cantidad, $id_producto;
     public $modal = false;
+ 
+    public function crear() {
+        $this->limpiar();
+        $this->abrirModal();
+    }
+    
+    public function abrirModal() {
+        $this->modal = true;
+    }
+    
+    public function cerrarModal() {
+        $this->modal = false;
+    }
+    
+    public function limpiar() {
+        $this->descripcion = "";
+        $this->cantidad = "";
+        $this->id_producto = "";
+    }
+    
+    public function guardar(){
+        $newProducto = new Producto();
+        $newProducto->descripcion = $this->descripcion;
+        $newProducto->cantidad = $this->cantidad;
+        $newProducto->save();
+        $this->limpiar();
+        $this->cerrarModal();
+        $this->productos = Producto::all();
+    }
+    
+    public function editar() {}
 
+    public function eliminar() {}
 
     public function render()
     {
         $this->productos = Producto::all();
         return view('livewire.productos');
     }
-
-    public function crear() {
-        $this->limpiar();
-        $this->abrirModal();
-    }
-
-    public function abrirModal() {
-        $this->modal = true;
-    }
-
-    public function cerrarModal() {
-        $this->modal = false;
-    }
-
-    public function limpiar() {
-        $this->descripcion = "";
-        $this->cantidad = "";
-        $this->id_producto = "";
-    }
-
 
 }
--- resources/views/livewire/productos.blade.php
+++ resources/views/livewire/productos.blade.php
@@ -5,7 +5,7 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-x1 sm:rounded-lg px-4 py-4">
 
-            <button wire:click="crear()" class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 my-3">Nuevo</button>
+            <button wire:click="crear()" class="bg-green-500 hover:bg-green-600 rounded text-white font-bold py-2 px-4 my-3">Nuevo</button>
             @if($modal)
                 @include('livewire.crear')
             @endif
@@ -15,6 +15,8 @@
                     <tr class="bg-red-600 text-white">
                         <th class="px-4 py-2 w-2/12">ID</th>
                         <th class="px-4 py-2">DESCRIPCION</th>
+                        <th class="px-4 py-2">CANTIDAD</th>
+                        <th class="px-4 py-2">ACCIONES</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -22,6 +24,11 @@
                     <tr>
                         <td class="border px-4 py-2 text-center w-2/12">{{$producto->id}}</td>
                         <td class="border px-4 py-2">{{$producto->descripcion}}</td>
+                        <td class="border text-center px-4 py-2">{{$producto->cantidad}}</td>
+                        <td class="border px-4 py-2 text-center">
+                            <button wire:click="editar({{$producto->id}})" class="bg-blue-500 hover:bg-blue-600 rounded text-white font-bold py-2 px-4">Editar</button>
+                            <button wire:click="eliminar({{$producto->id}})" class="bg-red-500 hover:bg-red-600 rounded text-white font-bold py-2 px-4">Eliminar</button>
+                        </td>
                     </tr>
                     @endforeach
                 </tbody>
